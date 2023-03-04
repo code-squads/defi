fcac3ef19f15048e2f709d00cf425ede6e678744
Rohan <rohan@Rohans-MacBook-Air-2.local>
2023-03-05 01:37:10 +0530

    landing start

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,16 +1,29 @@
 import Spline from "@splinetool/react-spline";
+import landingImg from "../assets/landingImg.jpg";
+import lendingSs from "../assets/lendingSs.jpeg";
+import Image from "next/Image";
 
 export default function App() {
   return (
-    <div className="">
-      <div className="fixed top-0 z-[-1] h-screen w-screen overflow-hidden">
-        <Spline scene="https://prod.spline.design/R7u20LxCiHGFBQGE/scene.splinecode" />
+    <div className="z-[-1] relative h-screen bg-[#12131A]">
+      <div className="z-[0] absolute top-0 h-screen w-screen  ">
+        <Spline
+          scene="https://prod.spline.design/R7u20LxCiHGFBQGE/scene.splinecode"
+          className="z-[0]"
+        />
       </div>
 
-      <div
-        className="z-10 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-[#362DB5] to-[#A047B0]"
-      >
-        lorejbafba jhaifbaiufgba jafb afaoi
+      <div className="flex flex-row items-center ">
+        <div className="z-[2] w-1/2 flex-1 m-16 relative font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-[#362DB5] to-[#A047B0]">
+          lorejbafba jhaifbaiufgba jafb afaoi
+        </div>
+        <div className="z-[2] w-1/2 flex-1 m-16 mr-10 relative">
+          <Image
+            src={lendingSs}
+            alt="gg"
+            className=" h-5/6 w-5/6  opacity-75 drop-shadow-xl rounded-lg"
+          />
+        </div>
       </div>
     </div>
   );

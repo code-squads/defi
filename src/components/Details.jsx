5f914b42f9f14da3424e31018d185075059c92b4
Vansh Baghel <vanshbaghel912@gmail.com>
2023-03-05 09:40:12 +0530

    added details of Details Page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import nft from "../assets/nft.jpeg";
+import matic from "../assets/matic.png";
+import usdc from "../assets/usdc.png";
+import ethLogo from "../assets/ethLogo.png";
 
 const Details = () => {
   return (
@@ -12,63 +14,77 @@ const Details = () => {
             // style={{ color: "rgba(54,45,101,0.7)" }}
           >
             {" "}
-            The leading non-custodial fixed yield rate protocol{" "}
+            Access the financial services with decentralized protocol{" "}
           </div>
           <div
             className="text-[18.7501px]"
             style={{ color: "rgb(111, 132, 152)" }}
           >
             {" "}
-            Here are a few other reasons why you should choose this DeFi{" "}
+            More about tokens supported by this DeFi
           </div>
         </div>
         <div className="w-full flex">
-          <div className="text-center flex-[0 0 33.33%] max-w-[33.33%] bg-[#000000] m-6 p-10 drop-shadow-[0_15px_20px_rgba(54,45,101,0.7)] opacity-70 rounded-xl">
+          <div className="text-center max-w-[33.33%] bg-[#000000] m-2 p-10 drop-shadow-[0_15px_20px_rgba(54,45,101,0.7)] opacity-70 rounded-xl">
             <div className="w-[4rem] border- h-[4rem] inline-block relative ">
-              <Image src={nft} alt="nft" className="flex justify-center align-middle" />
+              <Image
+                src={usdc}
+                alt="nft"
+                className="flex w-[50px] h-[50px] rounded-xl justify-center align-middle"
+              />
             </div>
             <div
               className="text-[17px]"
               style={{ color: "rgb(191, 191, 191)" }}
             >
-              Heading
+              USDC
             </div>
-            <div style={{ color: "#6f8498", fontSize: "1.2rem" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-              reiciendis commodi rem odit natus sunt cum nam illum optio nemo!
-              Sint sit veniam facilis quisquam dolores.
+            <div style={{ color: "#6f8498", letterSpacing: '2px', fontSize: "1.2rem" }}>
+              USDC is a fully-reserved stablecoin, which is a type of
+              cryptocurrency, or digital asset. Unlike other cryptocurrencies
+              that fluctuate in price, USDC is designed to maintain price
+              equivalence to the U.S. dollar
             </div>
           </div>
-          <div className="text-center p-10 flex-[0 0 33.33%] max-w-[33.33%] bg-[#000000] m-6 drop-shadow-[0_15px_20px_rgba(54,45,101,0.7)] opacity-70 rounded-xl">
+          <div className="text-center p-10 max-w-[33.33%] bg-[#000000] m-2 drop-shadow-[0_15px_20px_rgba(54,45,101,0.7)] opacity-70 rounded-xl">
             <div className="w-[4rem] border- h-[4rem] inline-block relative ">
-              <Image src={nft} alt="nft" className="flex justify-center align-middle" />
+              <Image
+                src={ethLogo}
+                alt="nft"
+                className="flex w-[50px] h-[50px] rounded-xl justify-center align-middle"
+              />
             </div>
             <div
               className="text-[17px]"
               style={{ color: "rgb(191, 191, 191)" }}
             >
-              Heading
+              Etherium
             </div>
-            <div style={{ color: "#6f8498", fontSize: "1.2rem" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-              reiciendis commodi rem odit natus sunt cum nam illum optio nemo!
-              Sint sit veniam facilis quisquam dolores.
+            <div style={{ color: "#6f8498", letterSpacing: '2px', fontSize: "1.2rem" }}>
+              Ethereum is a decentralized, open-source blockchain with smart
+              contract functionality. Ether is the native cryptocurrency of the
+              platform.
             </div>
           </div>
-          <div className="text-center flex-[0 0 33.33%] max-w-[33.33%] m-6 p-10 bg-[#000000] drop-shadow-[0_15px_20px_rgba(54,45,101,0.7)] opacity-70 rounded-xl">
+          <div className="text-center max-w-[33.33%] m-2 p-10 bg-[#000000] drop-shadow-[0_15px_20px_rgba(54,45,101,0.7)] opacity-70 rounded-xl">
             <div className="w-[4rem] border- h-[4rem] inline-block relative ">
-              <Image src={nft} alt="nft" className="flex justify-center align-middle" />
+              <Image
+                src={matic}
+                alt="nft"
+                className="flex w-[50px] h-[50px] rounded-xl justify-center align-middle"
+              />
             </div>
             <div
               className="text-[17px]"
               style={{ color: "rgb(191, 191, 191)" }}
             >
-              Heading
+              Matic
             </div>
-            <div style={{ color: "#6f8498", fontSize: "1.2rem" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-              reiciendis commodi rem odit natus sunt cum nam illum optio nemo!
-              Sint sit veniam facilis quisquam dolores.
+            <div style={{ color: "#6f8498", letterSpacing: '2px', fontSize: "1.2rem" }}>
+              Polygon (MATIC) is an Ethereum token that powers the Polygon
+              Network, a scaling solution for Ethereum. Polygon aims to provide
+              faster and cheaper transactions on Ethereum using Layer 2
+              sidechains
             </div>
           </div>
         </div>
